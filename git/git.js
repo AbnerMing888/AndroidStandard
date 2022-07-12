15817eb36e198b96dd5cfdf4cd108cc44e7dbc39
AbnerMing <598254259@qq.com>
2022-07-12 14:31:07 +0800

    提交index

--- git/git.js
+++ git/git.js
@@ -1,10 +1,10 @@
 let require = parent.window.require;
-const exec = require('child_process');
+const exec = require('child_process').exec;
 let fs = require('fs');
 $(function () {
     let selectPath = localStorage.getItem("select_path");
-    let cmdStr = "git log | grep '^Author: ' | awk '{print $2}' | sort | uniq -c | sort -k1,1nr";// -1 一个分支两次提交
-    //let cmdStr = "git log --format='%aN' | sort -u -";
+    //let cmdStr = "git log | grep '^Author: ' | awk '{print $2}' | sort | uniq -c | sort -k1,1nr";// -1 一个分支两次提交
+    let cmdStr = "git log  -p -2 --author='gongyaju' >>commit.diff";
     $(".getNowCommit").click(function () {
         //执行命令
         runExec();
@@ -12,7 +12,9 @@ $(function () {
 
     function runExec() {
         // 执行命令行，如果命令不需要路径，或就是项目根目录，则不需要cwd参数：
-        let workerProcess = exec(cmdStr, {cwd: selectPath})
+        let workerProcess = exec(cmdStr, {
+            cwd: selectPath}
+        );
         // 不受child_process默认的缓冲区大小的使用方法，没参数也要写上{}：workerProcess = exec(cmdStr, {})
         // 打印正常的后台可执行程序输出
         workerProcess.stdout.on('data', function (data) {
